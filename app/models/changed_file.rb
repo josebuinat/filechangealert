b2d0d8697571749f7f4af3dd19a8d0a65aa5b6cf
Jose Luis <josebuinat@hotmail.com>
2018-03-08 21:51:18 +1000

    add status to files

--- app/models/changed_file.rb
+++ app/models/changed_file.rb
@@ -1,12 +1,12 @@
 class ChangedFile < ApplicationRecord
   enum status: [
     :added, # file is added to the directory
-    :deleted, # file is deleted to the directory
+    :removed, # file is removed to the directory
     :modified, # file is modified to the directory
   ]
 
   scope :recent, -> { where(:created_at => 24.hours.ago..Time.now) }
 
-  def self.regist(files)
+  def self.regist(files, file_status)
   end
 end
--- app/services/run_listener.rb
+++ app/services/run_listener.rb
@@ -4,9 +4,9 @@ class RunListener
 
   def self.start
     listener = Listen.to(ENV["directory_path"]) do |modified, added, removed|
-      if added.count > 0
-        ChangedFile.regist(added)
-      end
+      if added.count > 0 ChangedFile.regist(added, :added)
+      if added.count > 0 ChangedFile.regist(modified, :modified)
+      if added.count > 0 ChangedFile.regist(removed, :removed)
     end
     listener.start
     listener.only /\.mov/
