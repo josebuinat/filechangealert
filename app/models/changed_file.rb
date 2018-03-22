72749eb124ecef0c57f9ce0d184bafad9373562e
Jose Luis <josebuinat@hotmail.com>
2018-03-22 13:06:00 +1000

    update loofa version to prevent a security vulnerability

--- app/models/changed_file.rb
+++ app/models/changed_file.rb
@@ -8,5 +8,12 @@ class ChangedFile < ApplicationRecord
   scope :recent, -> { where(:created_at => 24.hours.ago..Time.now) }
 
   def self.regist(files, file_status)
+    files.each do |f|
+      file = ChangedFile.new
+      file.file_name = File.basename(f)
+      file.file_path = f
+      file.status = file_status
+      file.save
+    end
   end
 end
